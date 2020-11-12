7854a15d2c38e8b753755a137e3ba43b6dd664f3
charlesx19 <charlesx19@gmail.com>
2020-11-12 18:37:39 +0800

    Refactoring #header for every pages, make it simple and beauty. Add remove Cart feature in Cart-panel and shoppingCart page.

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
 
+    showNews();
+
+    function showNews(){
+        setTimeout(function(){
+            $('#news-letter').addClass('open');
+            $('#darker-filter-news-letter').addClass('open');
+            $('body').addClass('modal-open');
+        }, 1000);
+    }
+
+    $('#darker-filter-news-letter').click(function(e){
+        $('#news-letter').removeClass('open');
+        $('#darker-filter-news-letter').removeClass('open');
+        $('body').removeClass('modal-open');
+    });
+
+    $('#news-letter .close').click(function(e){
+        $(e.currentTarget).closest('#news-letter').removeClass('open');
+        $('#darker-filter-news-letter').removeClass('open');
+        $('body').removeClass('modal-open');
+    });
+
+
     $('.carousel-featured').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
